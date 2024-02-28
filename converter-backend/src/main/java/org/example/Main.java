8f309691ae9f33e5aaff56900936a8044eceaac1
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-02-28 14:35:07 +0100

    Added Spring Boot and Swagger UI

--- converter-backend/src/main/java/org/example/Main.java
+++ converter-backend/src/main/java/org/example/Main.java
@@ -1,14 +1,19 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 import java.util.Map;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         String xmlFilePath = "src/main/resources/test-class-diagram.xml";
-        String outputPath = "src/main/resources/export.yml";
+        String outputPath = "src/main/resources/static/export.yml";
         Map<String, List<String>> entities = XMLParser.parseXML(xmlFilePath);
         OpenAPISpecGenerator.generateSpec(entities, outputPath);
     }
