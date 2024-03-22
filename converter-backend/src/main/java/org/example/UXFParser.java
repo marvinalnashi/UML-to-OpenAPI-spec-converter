d2ff11c3990adbac5099c3753d667fe9673ac766
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-03-23 00:09:16 +0100

    Update UXFParser.java

--- converter-backend/src/main/java/org/example/UXFParser.java
+++ converter-backend/src/main/java/org/example/UXFParser.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -24,15 +25,19 @@ public class UXFParser {
             NodeList nodeList = doc.getElementsByTagName("element");
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Element element = (Element) nodeList.item(i);
-                String type = element.getElementsByTagName("type").item(0).getTextContent();
+                NodeList typeList = element.getElementsByTagName("type");
+                if (typeList.getLength() > 0) {
+                    String type = typeList.item(0).getTextContent();
 
-                // Check if it's a class element
-                if ("com.baselet.element.old.element.Class".equals(type)) {
-                    // Panel attributes contain the class name and properties
-                    String panelAttributes = element.getElementsByTagName("panel_attributes").item(0).getTextContent();
-                    processPanelAttributes(panelAttributes, entities);
+                    // Process only class elements
+                    if ("com.baselet.element.old.element.Class".equals(type)) {
+                        NodeList panelAttributesList = element.getElementsByTagName("panel_attributes");
+                        if (panelAttributesList.getLength() > 0) {
+                            String panelAttributes = panelAttributesList.item(0).getTextContent();
+                            processPanelAttributes(panelAttributes, entities);
+                        }
+                    }
                 }
-                // Extend this condition to handle other types like relations if necessary
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -46,7 +51,6 @@ public class UXFParser {
             String className = lines[0].trim();
             List<String> properties = new ArrayList<>();
             for (int i = 1; i < lines.length; i++) {
-                // You can further refine this logic to separate properties and methods if needed
                 properties.add(lines[i].trim());
             }
             entities.put(className, properties);
